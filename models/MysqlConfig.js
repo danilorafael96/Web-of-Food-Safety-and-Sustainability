93983237e602d042a8c717422745a20632f0829b
Danilo Rafael <9danilorafael@gmail.com>
2020-03-23 22:47:52 +0000

    2ª entrega PBL

---
+++ models/MysqlConfig.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+// DBPASS must be defined with the database password
+// Powershell: $env:DBPASS="password"
+// CMD (windows): set DBPASS="password"
+// Linux and iOS: export DBPASS="password"
+// Heroku: Go to Settings and then to config vars
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host     : 'localhost',
+    user     : 'root',
+    password :  'Filho01',
+    database : 'newconection'
+});
+
+module.exports.pool = pool;
